d480303bf1f9774ecd51beb6f77a59e28e8eba49
Barrett <onkoe@protonmail.com>
2025-01-28 13:10:16 -0600

    feat: implement python api

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,23 @@
 //!
 //! ## Usage (Python)
 //!
-//! Unimplemented. This should use the `pyo3` feature to build, and I'll plan to upload it to PyPi soon.
+//! ```python
+//! from gps_rs import Gps
+//!
+//! # make a gps from an ip str + port int
+//! gps: Gps = Gps("192.168.1.222", 55556)
+//!
+//! # grab values from methods
+//! print(f"coord: {gps.coord()}")
+//! print(f"height: {gps.height()}")
+//! print(f"error: {gps.error()}")
+//! ```
+//!
+//! ## Development
+//!
+//! You'll want to get Rust and `maturin`. Download `cargo-binstall`, then run `cargo binstall maturin`.
+//!
+//! Afterwards, you can distribute
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
@@ -50,6 +66,7 @@ pub mod bindings {
 
 /// A safe wrapper for the low-level GPS functions.
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "python", pyo3::pyclass)]
 pub struct Gps {
     /// A pointer to a boxed character array on the heap.
     ///
@@ -128,6 +145,48 @@ impl Gps {
     }
 }
 
+// when python is turned on, we'll create an exception
+#[cfg(feature = "python")]
+pyo3::create_exception!(error, GpsException, pyo3::exceptions::PyException);
+
+#[cfg_attr(feature = "python", pyo3::pymethods)]
+impl Gps {
+    #[new]
+    pub fn py_new(ip: String, port: u16) -> pyo3::PyResult<Self> {
+        let ip = ip.parse()?;
+
+        Self::new(ip, port)
+            .inspect_err(|e| tracing::warn!("Failed to create GPS! err: {e}"))
+            .map_err(|e| GpsException::new_err(e.to_string()))
+    }
+
+    /// Finds the coordinate as determined by the GPS.
+    #[pyo3(name = "coord")]
+    pub fn py_coord(&self) -> Coordinate {
+        self.coord()
+    }
+
+    /// Finds the 'height', which is an arbitrary measure with WSG84.
+    ///
+    /// See the [`Height`] for additional information.
+    #[pyo3(name = "height")]
+    pub fn py_height(&self) -> Height {
+        self.height()
+    }
+
+    /// Gets the known 'error' in millimeters from the GPS.
+    #[pyo3(name = "error")]
+    pub fn py_error(&self) -> ErrorInMm {
+        self.error()
+    }
+}
+
+// SAFETY: we don't actually use the internal ptrs until we drop the class.
+unsafe impl Send for Gps {}
+
+// SAFETY: see above!
+unsafe impl Sync for Gps {}
+
 impl Drop for Gps {
     #[tracing::instrument(skip(self))]
     fn drop(&mut self) {
@@ -150,19 +209,46 @@ pub enum GpsError {
 ///
 /// The returned values are in degrees.
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "python", pyo3::pyclass)]
+#[cfg_attr(feature = "python", pyo3(eq))]
 pub struct Coordinate {
     pub lat: f64,
     pub lon: f64,
 }
 
+#[cfg_attr(feature = "python", pyo3::pymethods)]
+impl Coordinate {
+    pub fn __str__(&self) -> String {
+        format!("{}° lat, {}° lon", self.lat, self.lon)
+    }
+}
+
 /// Height, with the unit being "meters above a WGS84 ellipsoid".
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "python", pyo3::pyclass)]
+#[cfg_attr(feature = "python", pyo3(eq))]
 pub struct Height(pub f64);
+
+#[cfg_attr(feature = "python", pyo3::pymethods)]
+impl Height {
+    pub fn __str__(&self) -> String {
+        format!("{:.2}m", self.0)
+    }
+}
 
 /// Error in millimeters. Inside the `gps` library, this is calculated as the
 /// average of the vertical and horizontal 'error'.
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "python", pyo3::pyclass)]
+#[cfg_attr(feature = "python", pyo3(eq))]
 pub struct ErrorInMm(pub f64);
+
+#[cfg_attr(feature = "python", pyo3::pymethods)]
+impl ErrorInMm {
+    pub fn __str__(&self) -> String {
+        format!("{:.2}cm", self.0)
+    }
+}
 
 /*
 
@@ -201,3 +287,15 @@ impl Gps {
 pub struct TimeOfWeek(pub u32);
 
 */
+
+// export the module to python!
+#[cfg_attr(feature = "python", pyo3::pymodule)]
+#[cfg(feature = "python")]
+fn gps_rs(m: &pyo3::Bound<'_, pyo3::types::PyModule>) -> pyo3::PyResult<()> {
+    pyo3::types::PyModuleMethods::add_class::<Gps>(m)?;
+    pyo3::types::PyModuleMethods::add_class::<Coordinate>(m)?;
+    pyo3::types::PyModuleMethods::add_class::<Height>(m)?;
+    pyo3::types::PyModuleMethods::add_class::<ErrorInMm>(m)?;
+    pyo3::types::PyModuleMethods::add(m, "GpsException", m.py().get_type::<GpsException>())?;
+    Ok(())
+}
